d5cc690bcaad90506eb1652ad9f213f8fe147977
Julian Nunes <contato@juliannunes.com.br>
2023-09-15 22:18:18 -0300

    Ajustes sugeridos

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+function init() {
+  hideLoading();
+  loopChangingImg();
+  setCurrentYear();
+}
+
 function voltarFormulario() {
   window.scrollTo(0, 0);
 }
@@ -14,4 +20,19 @@ function hideLoading() {
 
   loadingContainer.classList.remove("d-block");
   loadingContainer.classList.add("d-none");
+}
+
+function changingImg() {
+  const imgs = document.querySelectorAll(".img-sobre-conteudo");
+  imgs.forEach((img) => img.classList.toggle("ativa"));
+}
+
+function loopChangingImg() {
+  setInterval(() => {
+    changingImg();
+  }, 5000);
+}
+
+function setCurrentYear() {
+  document.getElementById("current-year").innerText = new Date().getFullYear();
 }
